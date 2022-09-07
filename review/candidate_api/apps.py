97424167f85f117090a920ea57cc0633fef2ceff
soham-1 <sohampatkar93@gmail.com>
2022-09-07 10:49:36 +0530

    docker setup

---
+++ review/candidate_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CandidateApiConfig(AppConfig):
+    name = 'candidate_api'
---
+++ review/candidate_review/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CandidateReviewConfig(AppConfig):
+    name = 'candidate_review'
