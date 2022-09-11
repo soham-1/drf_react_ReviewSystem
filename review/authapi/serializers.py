2215cf45aaaa6b280f1e19a92d73406715da405b
soham-1 <sohampatkar93@gmail.com>
2022-09-11 19:27:12 +0530

    hash user password

--- review/authapi/serializers.py
+++ review/authapi/serializers.py
@@ -10,3 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def create(self, validated_data): # if password is not hashed then account is not considered active
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
